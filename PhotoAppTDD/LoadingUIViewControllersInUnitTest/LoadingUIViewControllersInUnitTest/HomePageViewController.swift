383d38b3774e43b9e21197477b9d88233bcd46f6
Roy Quesada <roy.quesada@gmail.com>
2023-05-29 14:32:00 -0600

    Different ways to load  ViewControllers

---
+++ PhotoAppTDD/LoadingUIViewControllersInUnitTest/LoadingUIViewControllersInUnitTest/HomePageViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomePageViewController.swift
+//  LoadingUIViewControllersInUnitTest
+//
+//  Created by Roy Quesada on 29/5/23.
+//
+
+import UIKit
+
+class HomePageViewController: UIViewController {
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+}
